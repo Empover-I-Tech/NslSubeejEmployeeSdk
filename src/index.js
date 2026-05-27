9a9c77cd1e3831250820323f53aa748a59a82ddb
kirant <kiran.t@empover.com>
2026-05-27 18:12:00 +0530

    Update entry point in package.json and fix export name in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 
 
-export { default as LoginScreen} from '../src/screens/LoginScreenRN'
+export { default as LoginScreenRn} from '../src/screens/LoginScreenRN'
 export { default as HomeScreenEmp} from '../src/screens/EmployeeScreens/HomeScreenEmp'
