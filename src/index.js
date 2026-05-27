e63b207fb74c82ea926cc806d7abc4875e727a8a
kirant <kiran.t@empover.com>
2026-05-27 18:48:10 +0530

    Add store and persistor exports to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
 
-export { default as LoginScreenRn} from '../src/screens/LoginScreenRN'
-export { default as HomeScreenEmp} from '../src/screens/EmployeeScreens/HomeScreenEmp'
+export { default as LoginScreenRn } from '../src/screens/LoginScreenRN'
+export { default as HomeScreenEmp } from '../src/screens/EmployeeScreens/HomeScreenEmp'
+export { store, persistor } from '../src/state/store'
