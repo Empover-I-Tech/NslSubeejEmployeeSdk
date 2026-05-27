97ba6fb24b41ba2d211e766bd932ff6a4f4a6ff9
kirant <kiran.t@empover.com>
2026-05-27 18:54:50 +0530

    Add setI18nConfig export to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@
 export { default as LoginScreenRn } from '../src/screens/LoginScreenRN'
 export { default as HomeScreenEmp } from '../src/screens/EmployeeScreens/HomeScreenEmp'
 export { store, persistor } from '../src/state/store'
+export { setI18nConfig } from '../src/Localization/Localisation'
