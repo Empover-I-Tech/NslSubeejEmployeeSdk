a7b49036f362da7349b23265951d5ed8d59270e2
kirant <kiran.t@empover.com>
2026-06-01 19:40:41 +0530

    Uncomment HomeScreenEmp and BottomTabsNavigatorEmp exports in index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ export { store, persistor } from './src/state/store';
 // ✅ Screens
 export { default as LoaderScreen }           from './src/LoaderScreen';
 // export { default as LoginScreenRn }          from './src/screens/LoginScreenRN';
-// export { default as HomeScreenEmp }          from './src/screens/EmployeeScreens/HomeScreenEmp';
-// export { default as BottomTabsNavigatorEmp } from './src/navigation/BottomTabsEmp';
+export { default as HomeScreenEmp }          from './src/screens/EmployeeScreens/HomeScreenEmp';
+export { default as BottomTabsNavigatorEmp } from './src/navigation/BottomTabsEmp';
