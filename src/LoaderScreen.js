49283c2a76bf2d2122d201e40091db7a9e14f72d
kirant <kiran.t@empover.com>
2026-06-01 14:37:25 +0530

    Update LoaderScreen to accept route props and display dynamic content

--- src/LoaderScreen.js
+++ src/LoaderScreen.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 
 
-const LoaderScreen = () => {
-
+const LoaderScreen = ({route}) => {
+    console.log("LoaderScreen route params:", route?.params);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{"Hello World"}</Text>
+            <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'black' }}>{route?.params?.navigateItem || "Hello World"}</Text>
         </View>
     )
 
