b8abe470118ba498db3c49ac1660b14fe9ffc190
kirant <kiran.t@empover.com>
2026-06-01 20:04:11 +0530

    fix: Uncomment BottomTabsNavigatorEmp export and add initLocalisation export in index.js

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ export { default as LoaderScreen }           from './src/LoaderScreen';
 // export { default as LoginScreenRn }          from './src/screens/LoginScreenRN';
 export { default as HomeScreenEmp }          from './src/screens/EmployeeScreens/HomeScreenEmp';
 export { default as BottomTabsNavigatorEmp } from './src/navigation/BottomTabsEmp';
+export { initLocalisation } from './src/Localization/Localisation';
