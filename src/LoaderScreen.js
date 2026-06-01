9178931470daa72e9c1552e469430bfc3939ad97
kirant <kiran.t@empover.com>
2026-06-01 14:34:34 +0530

    Refactor LoaderScreen layout for improved styling and structure

--- src/LoaderScreen.js
+++ src/LoaderScreen.js
@@ -4,9 +4,9 @@ import { View, Text } from 'react-native'
 const LoaderScreen = () => {
 
     return (
-        <View>
-            <Text>{"Hello World"}</Text>
-            </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{"Hello World"}</Text>
+        </View>
     )
 
 }
