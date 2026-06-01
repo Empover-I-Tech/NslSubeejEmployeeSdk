5d0a4aa54139500ff4c3aaa03f536f4fcc2aef4a
kirant <kiran.t@empover.com>
2026-06-01 16:40:15 +0530

    Reorganize exports in index.js for better clarity and structure

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// nslsubeejemployeesdk/index.js
 
-export { default as LoaderScreen } from './src/LoaderScreen'
-export { default as HomeScreenEmp} from './src/screens/EmployeeScreens/HomeScreenEmp'
+// ✅ Redux store
+export { store, persistor } from './src/state/store';
+
+// ✅ Screens
+export { default as LoaderScreen }           from './src/LoaderScreen';
+export { default as LoginScreenRn }          from './src/screens/LoginScreenRN';
+export { default as HomeScreenEmp }          from './src/screens/EmployeeScreens/HomeScreenEmp';
+export { default as BottomTabsNavigatorEmp } from './src/navigation/BottomTabsEmp';
