f7ebbb0bb8bb0fe3de0d469ac3517906cbeb59ab
kirant <kiran.t@empover.com>
2026-06-10 14:14:05 +0530

    add localization initialization in SubeejRoot component

--- src/SDKScreens/SubeejRoot.js
+++ src/SDKScreens/SubeejRoot.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { store, persistor } from '../state/store';
 import SubeejNavigator from './SubeejNavigator';
+import { initLocalisation } from '../Localization/Localisation';
 
 const SubeejRoot = (props) => {
+
+     useEffect(() => {
+        initLocalisation();
+    }, []);
     return (
         <Provider store={store}>
             <NavigationContainer independent={true}>
