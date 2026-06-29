7aeb0c406562f97ab21ec6d7bd519de82e66a5dd
kirant <kiran.t@empover.com>
2026-06-29 13:01:01 +0530

    fix: update import path for SubeejSDK in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // nslsubeejemployeesdk/index.js
 import 'react-native-get-random-values';
 
-export { default as SubeejSDK } from './src/SDKScreens/SubeejRoot';
+export { default as SubeejSDK } from './SDKScreens/SubeejRoot';
